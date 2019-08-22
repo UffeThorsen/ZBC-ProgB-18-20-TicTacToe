ecc728dc9e6dc2845cef606e661abbc5aee1af93
Tadayo <daniel.reffeldt@outlook.com>
2019-08-22 12:37:27 +0200

    Added Board Structure

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -3,5 +3,45 @@ namespace TicTacToe
 {
     class Board
     {
+
+        public enum Symbol
+        {
+            N,
+            O,
+            X
+        }
+
+        Symbol[,] places;
+
+        Board()
+        {
+
+        }
+
+        Board(int hori, int vert)
+        {
+
+        }
+
+        public bool ThreeInARow(int player)
+        {
+
+        }
+
+        public bool NInARow(int player, int length)
+        {
+            
+        }
+
+        public bool Place(int hor,int ver, int player)
+        {
+           
+        }
+
+        public override string ToString()
+        {
+
+        }
+
     }
 }
