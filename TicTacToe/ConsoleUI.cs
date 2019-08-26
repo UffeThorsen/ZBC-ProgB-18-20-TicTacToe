342e93f37d019835bc8968af5a8b989371ff9e34
tobi6593 <tobiofficial@outlook.com>
2019-08-26 13:07:38 +0200

    Udvidelse af main menu.

--- TicTacToe/ConsoleUI.cs
+++ TicTacToe/ConsoleUI.cs
@@ -4,6 +4,10 @@ namespace TicTacToe
 {
     class ConsoleUI
     {
+        public static void singlePLayer()
+        {
+
+        }
        public static void runGame()
         {
             Console.WriteLine("TicTacToe the game\n");
@@ -12,14 +16,24 @@ namespace TicTacToe
             Console.Clear();
             Console.WriteLine("1. Singleplayer");
             Console.WriteLine("2. 1v1");
+            Console.WriteLine("3. AI v AI");
             Console.WriteLine("3. Exit game");
             start:
             string input = Console.ReadLine().Trim().ToLower();            
             switch (input)
             {
                 case "1":
-                    Console.WriteLine("Press any key to start the game");
-                    Console.ReadKey();
+                    Console.WriteLine("1. 3 tokens with moves");
+                    Console.WriteLine("2. Unlimited tokens");
+                    Console.WriteLine("3. 3 tokens in order");
+                    string input1 = Console.ReadLine().Trim().ToLower();
+                    switch (input1)
+                    {
+                        case "1":
+                            Console.WriteLine("");
+                        break;
+                    }
+
                     //Start singleplayer game. 
                     break;
 
@@ -44,6 +58,12 @@ namespace TicTacToe
 
             
             Console.ReadKey();
-        }      
+        }
+        static void Main(string[] args)
+        {
+            runGame();
+        }
     }
+    
+    
 }
