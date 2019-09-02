f7a3915669e31febe502a3a711d501ac841547df
RedYodaDK <stigfrederiksen32@gmail.com>
2019-09-02 13:04:55 +0200

    Added Symbol Get to board interface

--- TicTacToe/Interface.cs
+++ TicTacToe/Interface.cs
@@ -6,6 +6,6 @@
         void RemoveSymbolFromPlace(int hor, int ver);
         bool BoardFull();
         string ToString();
-        //Symbol this[int i, int j];
+        Symbol this[int i, int j] { get; }
     }
 }
