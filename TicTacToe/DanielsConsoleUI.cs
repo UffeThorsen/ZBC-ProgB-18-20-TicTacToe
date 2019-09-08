743d592b05a6d1e2d28ccc55dabb66080a5bd232
Tadayo <daniel.reffeldt@outlook.com>
2019-09-08 16:07:37 +0200

    merge

---
+++ TicTacToe/DanielsConsoleUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+
+namespace TicTacToe
+{
+    
+    class DanielsConsoleUI
+    {
+
+        static int position = 2;
+
+        static public void PrintUI(Game game)
+        {
+            
+            while (true)
+            {
+                string[] temp = game.ToString().Split(' ');
+                temp[position] = "■";
+                WriteLine(ArrToSingleString(temp));
+                Thread.Sleep(50);
+                Clear();
+                WriteLine(game.ToString());
+                Thread.Sleep(100);
+            }
+        }
+
+        static string ArrToSingleString(string[] s)
+        {
+            string temp = "";
+            for (int i = 1; i <= s.Length; i++)
+            {
+
+                temp += s[i-1] + " ";
+
+                if (i % 3 == 0)
+                {
+                    temp += "\n";
+                }
+            }
+            return temp;
+        }
+
+    }
+
+
+}
