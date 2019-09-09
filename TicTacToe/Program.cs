dfe6fe301cda5f07571c9dbdceeb5fbc5a3c68de
RedYodaDK <stigfrederiksen32@gmail.com>
2019-09-09 13:58:02 +0200

    Made ConsoleUI static

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,7 +7,8 @@
         static void Main(string[] args)
         {
             //Running of the game! #FINALLY!
-           // ConsoleUI.RunGame();
+            ConsoleUI.RunGame(ConsoleUI.SetupGame());
+            
         }
     }
 }
