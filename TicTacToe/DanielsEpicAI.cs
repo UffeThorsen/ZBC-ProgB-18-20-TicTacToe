f80c7ad308f5c86beab1d7488c89b3bec8e6f008
Tadayo <daniel.reffeldt@outlook.com>
2019-09-12 12:43:00 +0200

    Added a more epic ai

--- TicTacToe/DanielsEpicAI.cs
+++ TicTacToe/DanielsEpicAI.cs
@@ -6,22 +6,54 @@ using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    class DanielsEpicAI :IPlayer
+
+    class NoAvailableMoveException : ApplicationException
     {
-        public Placement NextMove(Game gameState)
+        public NoAvailableMoveException()
+        {
+
+        }
+    }
+
+    class DanielsEpicAI : IPlayer
+    {
+
+        protected Placement FirstAvaiableMovement(Game gameState)
         {
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if (gameState.IsLegalMove(new Placement(i,j)))
+                    if (gameState.IsLegalMove(new Placement(i, j)))
                     {
-                        return new Placement(i,j);
+                        return new Placement(i, j);
                     }
                 }
-                
+
             }
-            throw new Exception();
+            throw new NoAvailableMoveException();
+        }
+
+        public Placement NextMove(Game gameState)
+        {
+            return FirstAvaiableMovement(gameState);
         }
     }
+
+    class DanielsEpicerAI : DanielsEpicAI
+    {
+        public Placement NextMove(Game gameState)
+        {
+            if(gameState.IsLegalMove(new Placement(0,2)))
+            {
+                return new Placement(0, 2);
+            }
+            else if (gameState.IsLegalMove(new Placement(2, 0)))
+            {
+                return new Placement(2, 0);
+            }
+            return FirstAvaiableMovement(gameState);
+        }
+   }
+
 }
