de8031f4b0c8980ce6ed1de3757a7e2cff12a729
bwolfed <patricknie9100@gmail.com>
2019-09-16 14:08:10 +0200

    TrailAI

---
+++ TicTacToe/AI/PoppeAi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.AI
+{
+    class PoppeAi : IPlayer
+    {
+        private Random r = new Random();
+
+        protected Placement RandomMove(Game gamestate)
+        {
+            int i = r.Next(3);
+            int j = r.Next(3);
+
+            Placement p = new Placement(i, j);
+            while (!gamestate.IsLegalMove(p))
+            {
+                p.X = r.Next(3);
+                p.Y = r.Next(3);
+            }
+            return p;
+        }
+
+        public virtual Placement NextMove(Game gamestate)
+        {
+            return RandomMove(gamestate);
+        }
+    }
+
+      /*class PoppeUnbeatableAI
+  {
+        enum Player : Int32
+        {
+        }
+
+        private Func minimax(Board: Array<Player>, IPlayer: Player)  Int32{
+            if let winner = .getWinner(Board) as IPlayer
+    }*/
+}
