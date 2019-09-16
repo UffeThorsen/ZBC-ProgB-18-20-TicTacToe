9dd61adaa27e63a532b9b4edaa78ab26bfa9499b
Tomsten <tomsten.2000@hotmail.com>
2019-09-16 13:29:51 +0200

    Fix ThomasAIPlayer

--- TicTacToe/AI/ThomasAIPlayer.cs
+++ TicTacToe/AI/ThomasAIPlayer.cs
@@ -12,48 +12,48 @@ namespace TicTacToe
 
         public Placement NextMove(Game gameState)
         {
-            if(TwoInARow(gameState, 1, 0, 2, 0) || TwoInARow(gameState, 0, 1, 0, 2) || TwoInARow(gameState, 1, 1, 2, 2))
+            if (TwoInARow(gameState, 1, 0, 2, 0) || TwoInARow(gameState, 0, 1, 0, 2) || TwoInARow(gameState, 1, 1, 2, 2))
             {
-                return (0, 0);
+                return new Placement(0, 0);
             }
             else if (TwoInARow(gameState, 1, 1, 1, 2))
             {
-                return (0, 1);
+                return new Placement(0, 1);
             }
             else if (TwoInARow(gameState, 0, 0, 1, 0) || TwoInARow(gameState, 2, 1, 2, 2) || TwoInARow(gameState, 0, 2, 1, 1))
             {
-                return (2, 0);
+                return new Placement(2, 0);
             }
             else if (TwoInARow(gameState, 1, 1, 1, 2))
             {
-                return (1, 0);
+                return new Placement(1, 0);
             }
             else if (TwoInARow(gameState, 0, 1, 1, 1))
             {
-                return (2, 1);
+                return new Placement(2, 1);
             }
             else if (TwoInARow(gameState, 1, 2, 2, 2) || TwoInARow(gameState, 0, 0, 0, 1) || TwoInARow(gameState, 1, 1, 2, 0))
             {
-                return (0, 2);
+                return new Placement(0, 2);
             }
             else if (TwoInARow(gameState, 1, 0, 1, 1))
             {
-                return (1, 2);
+                return new Placement(1, 2);
             }
             else if (TwoInARow(gameState, 0, 2, 1, 2) || TwoInARow(gameState, 0, 0, 1, 1) || TwoInARow(gameState, 2, 0, 2, 1))
             {
-                return (2, 2);
+                return new Placement(2, 2);
             }
             else
             {
                 RandomMove(gameState);
             }
-
+            return new Placement();//for compiler only
         }
 
         private bool TwoInARow(Game gameState, int x1, int y1, int x2, int y2)
         {
-            if(gameState.gameBoard.Symbol(x1, y1) == Symbol.X && gameState.gameBoard.Symbol(x2, y2) == Symbol.X || gameState.gameBoard.Symbol(x1, y1) == Symbol.O && gameState.gameBoard.Symbol(x2, y2) == Symbol.O)
+            if(gameState[x1, y1] == Symbol.X && gameState[x2, y2] == Symbol.X || gameState[x1, y1] == Symbol.O && gameState[x2, y2] == Symbol.O)
             {
                 return true;
             }
@@ -74,7 +74,7 @@ namespace TicTacToe
                 p.X = r.Next(3);
                 p.Y = r.Next(3);
             }
-
+            return p;
         }
     }
 }
--- TicTacToe/ConsoleUI.cs
+++ TicTacToe/ConsoleUI.cs
@@ -13,7 +13,7 @@ namespace TicTacToe
             typeof(DanielsEpicAI),
             typeof(DanielsEpicerAI),
             //typeof(JacobIAI),
-            //typeof(ThomasAIPlayer),
+            typeof(ThomasAIPlayer),
             typeof(RasmusAI),
             typeof(StigAI),
             typeof(KasperAI)
