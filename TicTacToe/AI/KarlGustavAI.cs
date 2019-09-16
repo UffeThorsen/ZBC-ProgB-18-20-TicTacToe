146bfab08ccea96d9487a67ac9542ec2cb8fb78e
PaleElf <karl0899@gmail.com>
2019-09-16 14:02:20 +0200

    add ai

---
+++ TicTacToe/AI/KarlGustavAI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class KarlGustavAI : IPlayer
+    {
+        public Placement NextMove(Game gameState)
+        {
+            int x;
+            int y;
+            Placement place = new Placement (0, 0);
+            while (!gameState.IsLegalMove(place))
+            {
+                for (int i = 0; i > 3; i++)
+                {
+                    Placement p = new Placement(x, y);
+                    if (gameState.IsLegalMove(p))
+                    {
+                        return p;
+                    }
+                    y++;
+
+                }
+                y = 0;
+                x++;
+            }
+            return place;
+        }
+    }
+}
