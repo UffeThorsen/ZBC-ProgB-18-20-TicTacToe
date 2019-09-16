2cd9f7af4357cb96d2da5df8cd3b60b89dbdad50
LittleSchnitz <kaspertrantel@gmail.com>
2019-09-16 13:27:42 +0200

    Fixed KasperAI
    Fixed namespace
    Implemented IPlayer

--- TicTacToe/AI/KasperAI.cs
+++ TicTacToe/AI/KasperAI.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TicTacToe.AI
+namespace TicTacToe
 {
-    class KasperAI
+    class KasperAI : IPlayer
     {
         public virtual string Description
         {
