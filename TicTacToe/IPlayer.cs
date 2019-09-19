2b23418926840cd0496767def8d391564f3053e4
Uffe Thorsen <ivanho@ivanho.dk>
2019-09-19 12:34:08 +0200

    Moved Placement to own file.

--- TicTacToe/IPlayer.cs
+++ TicTacToe/IPlayer.cs
@@ -1,22 +1,6 @@
 ﻿
 namespace TicTacToe
 {
-    public struct Placement {
-        public int X { set; get; }
-        public int Y { set; get; }
-
-        public Placement(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public override string ToString()
-        {
-            return "(" + X + ", " + Y + ")";
-        }
-    }
-
     public interface IPlayer
     {
         Placement NextMove(Game gameState);
