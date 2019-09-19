aed210841506f732d7447687ab9b77e4ea769a39
Uffe Thorsen <ivanho@ivanho.dk>
2019-09-19 09:39:28 +0200

    Uffe set up test.

---
+++ UnitTestTicTacToe/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTicTacToe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTicTacToe")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67013cae-1053-4422-bc7f-4ab19fe2b70e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestTicTacToe/UnitTestBoard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+
+namespace UnitTestTicTacToe
+{
+    [TestClass]
+    public class UnitTestBoard
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
