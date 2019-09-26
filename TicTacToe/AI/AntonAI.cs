56ccb3ae4b78f8b44aae02d5501722dc6cdc796c
Antonk123 <anton.krath@gmail.com>
2019-09-26 11:17:58 +0200

    added AntonAI

---
+++ TicTacToe/AI/AntonAI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.AI
+{
+    class AntonAI
+    {
+        private Random r = new Random();
+
+        Placement NextMove(Game gameState)
+        {
+            int i = r.Next(3);
+            int j = r.Next(3);
+            Placement p = new Placement(i, j);
+            while (!gameState.IsLegalMove(p))
+            {
+                p.X = r.Next(3);
+                p.Y = r.Next(3);
+            }
+            return p;
+        }
+    }
+}
