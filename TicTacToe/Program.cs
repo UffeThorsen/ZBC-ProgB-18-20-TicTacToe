90d5c01b4e465356163958039393e21510fe6ecc
Uffe Thorsen <ivanho@ivanho.dk>
2019-11-14 11:10:07 +0100

    Added check for unique objects in Game constructor.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,6 +6,8 @@
 
         static void Main(string[] args)
         {
+            //AITournament.RunLeague();
+            //AITournament.RunNLeagues(10);
             //Running of the game! #FINALLY!
             Game game = ConsoleUI.SetupGame();
             ConsoleUI.RunGame(game);
