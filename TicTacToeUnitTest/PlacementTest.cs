2aacb3c525387af67cb021540345caba7bea75a8
Uffe Thorsen <ivanho@ivanho.dk>
2019-11-14 09:40:05 +0100

    Full test of Placement.

---
+++ TicTacToeUnitTest/PlacementTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+
+namespace TicTacToeUnitTest
+{
+    [TestClass]
+    public class PlacementTest
+    {
+        [TestMethod]
+        public void TestPlacementDefaultConstructor()
+        {
+            Placement p = new Placement();
+        }
+
+        [TestMethod]
+        [DataRow(26, -474)]
+        public void TestPlacementConstructor(int x, int y)
+        {
+            Placement p = new Placement(x, y);
+            Assert.AreEqual(x, p.X);
+            Assert.AreEqual(y, p.Y);
+        }
+
+        [TestMethod]
+        [DataRow(-13, 272)]
+        [DataRow(0, 0)]
+        public void TestSetGet(int x, int y)
+        {
+            Placement p = new Placement();
+            p.X = x;
+            p.Y = y;
+            Assert.AreEqual(x, p.X);
+            Assert.AreEqual(y, p.Y);
+        }
+
+        [TestMethod]
+        [DataRow(-13, 272)]
+        [DataRow(0, 0)]
+        public void TestToString(int x, int y)
+        {
+            Placement p = new Placement(x,y);
+            string expected = "(" + x + ", " + y + ")";
+            Assert.AreEqual(expected, p.ToString());
+        }
+    }
+}
