1bbcecdc2a2d44666772fa684e2659afb9942966
Uffe Thorsen (UTHO - Adjunkt - RIAH - ZBC) <utho@zbc.dk>
2019-11-14 10:44:38 +0100

    Fixed placement test.

--- TicTacToeUnitTest/PlacementTest.cs
+++ TicTacToeUnitTest/PlacementTest.cs
@@ -23,6 +23,22 @@ namespace TicTacToeUnitTest
         }
 
         [TestMethod]
+        [DataRow(26, -474)]
+        public void TestGetXAfterConstruct(int x, int y)
+        {
+            Placement p = new Placement(x, y);
+            Assert.AreEqual(x, p.X);
+        }
+
+        [TestMethod]
+        [DataRow(26, -474)]
+        public void TestGetYAfterConstruct(int x, int y)
+        {
+            Placement p = new Placement(x, y);
+            Assert.AreEqual(y, p.Y);
+        }
+
+        [TestMethod]
         [DataRow(-13, 272)]
         [DataRow(0, 0)]
         public void TestSetGet(int x, int y)
