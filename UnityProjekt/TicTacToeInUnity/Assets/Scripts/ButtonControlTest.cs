5dfbd6dd427a823e0995f685e4bfd94116958670
RedYodaDK <stigfrederiksen32@gmail.com>
2019-12-03 15:02:34 +0100

    Final UI

--- UnityProjekt/TicTacToeInUnity/Assets/Scripts/ButtonControlTest.cs
+++ UnityProjekt/TicTacToeInUnity/Assets/Scripts/ButtonControlTest.cs
@@ -10,11 +10,12 @@ public class ButtonControlTest : MonoBehaviour
     public Game currentGame;
     public IPlayer player1;
     public IPlayer player2;
-    public Text currentPLayerText;
+    public Text currentPlayerText;
     public Text[,] playingFieldText = new Text[3,3];
+    public Transform[,] playingFieldButtons = new Transform[3,3];
     public GameObject playingBoard;
-    public int x;
-    public int y;
+    public int[] lastPos = new int[2];
+    bool someoneHasWon = false;
 
     void Start()
     {
@@ -24,6 +25,7 @@ public class ButtonControlTest : MonoBehaviour
             for (int j = 0; j < 3; j++)
             {
                 playingFieldText[i, j] = playingBoard.transform.GetChild(k).GetChild(0).GetComponent<Text>();
+                playingFieldButtons[i, j] = playingBoard.transform.GetChild(k);
                 k++;
             }
         }
@@ -31,9 +33,9 @@ public class ButtonControlTest : MonoBehaviour
 
     void Update()
     {
-        if (currentGame != null)
+        if (currentGame != null && !someoneHasWon)
         {
-            currentPLayerText.text = "Current player: " + currentGame.CurrentPlayer.ToString();
+            currentPlayerText.text = "Current player: " + currentGame.CurrentPlayer.ToString();
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
@@ -48,9 +50,58 @@ public class ButtonControlTest : MonoBehaviour
     {
         player1 = new UffeAIPlayer();
         player2 = new UffeAIPlayer();
+       /* player1 = new UiIplayer(this);
+        player2 = new UiIplayer(this);*/
         currentGame = new Game(player1,player2);
     }
+
+    
+    public Placement LastValidButtonPress()
+    {
+        return new Placement(lastPos[0],lastPos[1]);
+    }
+
+    public void BoardPressed(Button b)
+    {
+        if (b.GetComponent<Text>().text == Symbol.N.ToString())
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (b.transform == playingFieldButtons[i,j])
+                    {
+                        lastPos[0] = i;
+                        lastPos[1] = j;
+                    }
+                }
+            }
+        }
+    }
+
+    public void DoNextMove()
+    {
+        if (!currentGame.IsGameDone)
+        {
+            currentGame.NextMove();
+        }
+        else
+        {
+            someoneHasWon = true;
+            if (currentGame.WhoWon() == null)
+            {
+                currentPlayerText.text = "The game was a tie";
+            }
+            else
+            {
+                currentPlayerText.text = "Winning player: " + currentGame.WhoWon();
+            }
+        }
+    }
+
     /*
+    public int x;
+    public int y;
     public void PlaceTest()
     {
         currentGame[x,y] = currentGame.CurrentPlayer; 
---
+++ UnityProjekt/TicTacToeInUnity/Assets/Scripts/UiIplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TicTacToe;
+
+
+public class UiIplayer : IPlayer
+{
+
+    ButtonControlTest but;
+
+    public UiIplayer(ButtonControlTest b)
+    {
+        but = b;
+    }
+
+    public Placement NextMove(Game gameState)
+    {
+        return but.LastValidButtonPress();   
+    }
+
+}
