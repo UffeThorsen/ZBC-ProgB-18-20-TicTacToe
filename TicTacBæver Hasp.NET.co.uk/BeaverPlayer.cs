0aa3290ea550fe4212d90c41c12c908f4d03ffc9
rasmusdas <rasmusmkn@gmail.com>
2019-12-03 13:51:22 +0100

    Added human controllability functionality Split code into multiple files (it was hell) Added BeaverPlayer.cs Added BeaverUI.cs

---
+++ TicTacBæver Hasp.NET.co.uk/BeaverPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicTacToe;
+
+namespace TicTacBæver_Hasp.NET.co.uk
+{
+
+    public class BeaverPlayer : IPlayer
+    {
+        public static Placement nextMove;
+
+        public Placement NextMove(Game game)
+        {
+            return nextMove;
+        }
+    }
+}
---
+++ TicTacBæver Hasp.NET.co.uk/BeaverUI.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using TicTacBæver_Hasp.NET.co.uk;
+using TicTacBæver_Hasp.NET.co.uk.Shared;
+using TicTacToe;
+
+namespace TicTacBæver_Hasp.NET.co.uk
+{
+    public class BeaverUI
+    {
+        static IPlayer a = new BeaverPlayer();
+        static IPlayer b = new UffeSmarterAIPlayer();
+        public static Game game = new Game(a, b);
+        static bool timerStarted = false;
+        static bool hasClicked = false;
+        public static Placement nextPlayerMove;
+        public static Action pageUpdate = null;
+
+        public static string SymbolToImage(Symbol s)
+        {
+            if (s == Symbol.X) return "/beaver.png";
+            if (s == Symbol.O) return "/baevreasp.png";
+            return "/aa.png";
+        }
+
+        public static void MakeMove(int i, int j)
+        {
+            nextPlayerMove = new Placement(i, j);
+            hasClicked = true;
+        }
+
+        public static void StartNewGame()
+        {
+            if (!timerStarted)
+            {
+                game = new Game(a, b);
+                timerStarted = true;
+                System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ThreadStart(Timer));
+                thread.Start();
+            }
+        }
+
+        public static void Timer()
+        {
+            while (!game.IsGameDone)
+            {
+                // TO DO : ADD COMMENTS
+                if (game.CurrentPlayer == Symbol.X && a.GetType() == typeof(BeaverPlayer) || (game.CurrentPlayer == Symbol.O && b.GetType() == typeof(BeaverPlayer)))
+                {
+                    hasClicked = false;
+                    while (!hasClicked)
+                    {
+
+                    }
+                    hasClicked = false;
+                    BeaverPlayer.nextMove = nextPlayerMove;
+                    if (game.IsLegalMove(BeaverPlayer.nextMove))
+                    {
+                        game.NextMove();
+                    }
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(100);
+                    game.NextMove();
+                }
+                pageUpdate.Invoke();
+            }
+            timerStarted = false;
+        }
+    }
+}
