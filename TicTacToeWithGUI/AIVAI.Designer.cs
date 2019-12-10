3247ccf82e2611542aeedefbdee9c4d2629c5a32
tobi6593 <tobiofficial@outlook.com>
2019-12-10 13:21:35 +0100

    Made Layout to PVP

--- TicTacToeWithGUI/AIVAI.Designer.cs
+++ TicTacToeWithGUI/AIVAI.Designer.cs
@@ -39,10 +39,13 @@
             this._01 = new System.Windows.Forms.Button();
             this._00 = new System.Windows.Forms.Button();
             this._22 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.win = new System.Windows.Forms.Label();
             this.mainmenu = new System.Windows.Forms.Button();
             this.pt = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
@@ -67,84 +70,76 @@
             // 
             // _21
             // 
-            this._21.Location = new System.Drawing.Point(584, 311);
+            this._21.Location = new System.Drawing.Point(598, 315);
             this._21.Name = "_21";
-            this._21.Size = new System.Drawing.Size(44, 42);
+            this._21.Size = new System.Drawing.Size(60, 60);
             this._21.TabIndex = 28;
             this._21.UseVisualStyleBackColor = true;
             // 
             // _20
             // 
-            this._20.Location = new System.Drawing.Point(534, 311);
+            this._20.Location = new System.Drawing.Point(532, 315);
             this._20.Name = "_20";
-            this._20.Size = new System.Drawing.Size(44, 42);
+            this._20.Size = new System.Drawing.Size(60, 60);
             this._20.TabIndex = 27;
             this._20.UseVisualStyleBackColor = true;
             // 
             // _10
             // 
-            this._10.Location = new System.Drawing.Point(534, 263);
+            this._10.Location = new System.Drawing.Point(532, 249);
             this._10.Name = "_10";
-            this._10.Size = new System.Drawing.Size(44, 42);
+            this._10.Size = new System.Drawing.Size(60, 60);
             this._10.TabIndex = 26;
             this._10.UseVisualStyleBackColor = true;
             // 
             // _11
             // 
-            this._11.Location = new System.Drawing.Point(584, 263);
+            this._11.Location = new System.Drawing.Point(598, 249);
             this._11.Name = "_11";
-            this._11.Size = new System.Drawing.Size(44, 42);
+            this._11.Size = new System.Drawing.Size(60, 60);
             this._11.TabIndex = 25;
             this._11.UseVisualStyleBackColor = true;
             // 
             // _12
             // 
-            this._12.Location = new System.Drawing.Point(634, 263);
+            this._12.Location = new System.Drawing.Point(664, 249);
             this._12.Name = "_12";
-            this._12.Size = new System.Drawing.Size(44, 42);
+            this._12.Size = new System.Drawing.Size(60, 60);
             this._12.TabIndex = 24;
             this._12.UseVisualStyleBackColor = true;
             // 
             // _02
             // 
-            this._02.Location = new System.Drawing.Point(634, 215);
+            this._02.Location = new System.Drawing.Point(664, 183);
             this._02.Name = "_02";
-            this._02.Size = new System.Drawing.Size(44, 42);
+            this._02.Size = new System.Drawing.Size(60, 60);
             this._02.TabIndex = 23;
             this._02.UseVisualStyleBackColor = true;
             // 
             // _01
             // 
-            this._01.Location = new System.Drawing.Point(584, 215);
+            this._01.Location = new System.Drawing.Point(598, 183);
             this._01.Name = "_01";
-            this._01.Size = new System.Drawing.Size(44, 42);
+            this._01.Size = new System.Drawing.Size(60, 60);
             this._01.TabIndex = 22;
             this._01.UseVisualStyleBackColor = true;
             // 
             // _00
             // 
-            this._00.Location = new System.Drawing.Point(534, 215);
+            this._00.Location = new System.Drawing.Point(532, 183);
             this._00.Name = "_00";
-            this._00.Size = new System.Drawing.Size(44, 42);
+            this._00.Size = new System.Drawing.Size(60, 60);
             this._00.TabIndex = 21;
             this._00.UseVisualStyleBackColor = true;
             this._00.Click += new System.EventHandler(this._00_Click);
             // 
             // _22
             // 
-            this._22.Location = new System.Drawing.Point(634, 312);
+            this._22.Location = new System.Drawing.Point(664, 315);
             this._22.Name = "_22";
-            this._22.Size = new System.Drawing.Size(44, 42);
+            this._22.Size = new System.Drawing.Size(60, 60);
             this._22.TabIndex = 20;
             this._22.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel1.Location = new System.Drawing.Point(505, 195);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 179);
-            this.panel1.TabIndex = 29;
             // 
             // win
             // 
@@ -176,11 +171,47 @@
             this.pt.TabIndex = 34;
             this.pt.Text = " ";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(592, 184);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 190);
+            this.panel1.TabIndex = 35;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(658, 184);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 191);
+            this.panel2.TabIndex = 36;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.Location = new System.Drawing.Point(533, 243);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(191, 5);
+            this.panel3.TabIndex = 37;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Location = new System.Drawing.Point(533, 309);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(191, 5);
+            this.panel4.TabIndex = 38;
+            // 
             // AIVAI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 596);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pt);
             this.Controls.Add(this.mainmenu);
             this.Controls.Add(this.win);
@@ -194,7 +225,6 @@
             this.Controls.Add(this._01);
             this.Controls.Add(this._00);
             this.Controls.Add(this._22);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "AIVAI";
             this.Text = "Form1";
@@ -217,9 +247,12 @@
         private System.Windows.Forms.Button _01;
         private System.Windows.Forms.Button _00;
         private System.Windows.Forms.Button _22;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label win;
         private System.Windows.Forms.Button mainmenu;
         private System.Windows.Forms.Label pt;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
