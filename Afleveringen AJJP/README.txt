19f1622f8b512f133fdd650df4ff49dd197ce7b2
JHJS140901 <flamewings1409@gmail.com>
2019-12-17 22:32:35 +0100

    Making the assignment folders and making it ready for .zip compression.

---
+++ Afleveringen AJJP/README.txt
@@ -0,0 +1,25 @@
+Velkommen til TicTacToe.
+Dette er et windows forms GUI (grafical user interface, eller også kaldet grafisk brugerflade) for spillet TicTacToe.
+
+- Hoved Menuen -
+Når man åbner programmet, så vil der står vores titel og 3 knapper under den.
+Knap 1 er, hvor spillet et med 2 "Human" spillere.
+Knap 2 er, hvor spillet er med 1 "Human" spiller og en AI spiller.
+Knap 3 er, hvor spillet er med 2 AI spillere.
+Har man trykket på en af knapperne, så vil spillet starte ud fra den valgte spiltype.
+
+- Menu Strip - 
+Oppe i toppen af programmet, så vil der (i starten af programmet) stå "file", "about" og "mode".
+Trykker man på "file" kan man vælge et nyt spil eller lukke programmet.
+Trykker man på "about", så popper der en ny "form" op, hvori der står hvem der har lavet programmet og hvornår det blev afleveret.
+Dog kan man ikke trykke på "mode", da det er noget der kun ændres efter at man har valgt hvilken spil-type man vil spille med.
+F.eks. vælger man at spille "player vs. player", så vil der stå "player vs. player" i stedet for "mode".
+
+- Spillet -
+Vælger man "player vs. player", så kommer man videre ved at trykke på en af de 9 centrale knapper der danner en 3x3 firkant.
+Vælger man "player vs. AI", så vil man skulle trykke på en af de 9 centrale knapper og derefter trykke på "continue" knappen for at AI'en gør sit træk.
+Vælger man "AI vs. AI", så skal man kun trykke "continue" knappen, for at se hvert træk de 2 AI'er udfører.1
+
+- Spillet er ovre -
+Man kan hele tiden trykke på "Restart" knappen under spillet eller når spillet er færdigt.
+Derudover kan man altid gå under filer og vælge "new game" og derefter vælge sin nye spil-type.
---
+++ Afleveringen AJJP/src/AJJP_TicTacToe.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToe;
+
+namespace TicTacToe_WindsForms
+{
+    public partial class AJJP_TicTacToe : Form
+    {
+        public Game game { get; set; }
+        public static Placement nextP;
+
+        Dictionary<Button, Placement> buttonsPlacements = new Dictionary<Button, Placement>();
+
+        private bool gameStarted = false;
+        private int gameMode = 0;
+
+        public AJJP_TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void AJJP_TicTacToe_Load(object sender, EventArgs e)
+        {
+            //Loads the different button placements into the dictionary
+            Placement pA1 = new Placement(0, 0);
+            Placement pA2 = new Placement(0, 1);
+            Placement pA3 = new Placement(0, 2);
+            Placement pB1 = new Placement(1, 0);
+            Placement pB2 = new Placement(1, 1);
+            Placement pB3 = new Placement(1, 2);
+            Placement pC1 = new Placement(2, 0);
+            Placement pC2 = new Placement(2, 1);
+            Placement pC3 = new Placement(2, 2);
+
+            buttonsPlacements[A1_btn] = pA1;
+            buttonsPlacements[A2_btn] = pA2;
+            buttonsPlacements[A3_btn] = pA3;
+            buttonsPlacements[B1_btn] = pB1;
+            buttonsPlacements[B2_btn] = pB2;
+            buttonsPlacements[B3_btn] = pB3;
+            buttonsPlacements[C1_btn] = pC1;
+            buttonsPlacements[C2_btn] = pC2;
+            buttonsPlacements[C3_btn] = pC3;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This game have been made by Anton, Jacob A, Jannie and Patrick 3.P. \nDelivered 20/12-2019.", "Info about game");
+        }
+
+        //Here begins the code for the game
+        private void NextStep()
+        {
+            if (!game.IsGameDone) //Runs only if the game is still going
+            {
+                game.NextMove(); //Changes the turn
+                TurnText.Text = "Turn: " + game.CurrentPlayer.ToString(); //Shows the next player
+
+                UpdateForm(); //Updates the form with the right symbols
+            }
+
+            if (game.WhoWon() == null && game.IsGameDone) //If the game ends in a tie and is done
+            {
+                TurnText.Text = "Tie";
+                gameStarted = false;
+            }
+            else if (game.WhoWon() != null) //If the game has a winner
+            {
+                if(game.CurrentPlayer == Symbol.X)
+                {
+                    TurnText.Text = "O Has won!";
+                }
+                else
+                {
+                    TurnText.Text = "X Has won!";
+                }
+                
+                //TurnText.Text = game.WhoWon().ToString(); //Tells who won the game
+                gameStarted = false;
+            }
+        }
+
+        private void UpdateForm()
+        {
+            int index = 0;
+            for (int i = 0; i < 3; i++) //Checks through the vertical placement spaces
+            {
+                for (int j = 0; j < 3; j++) //Checks through the horizontal placement spaces
+                {
+                    //Changes the buttons text element to the right symbol
+                    buttonsPlacements.Keys.ToArray()[index].Text = game[i,j].ToString();
+                    index++;
+                }
+            }
+        }
+
+        private void Continue_btn_Click(object sender, EventArgs e)
+        {
+            if (gameStarted)
+            {
+                if (gameMode == 2 && game.CurrentPlayer != Symbol.X)
+                {
+                    NextStep();
+                }
+                else if (gameMode == 3)
+                {
+                    NextStep();
+                }
+            }
+        }
+
+        private void GameModeChosen()
+        {
+            Restart_btn.Visible = true;
+            Continue_btn.Visible = true;
+            Board.Visible = true;
+            Mode1.Visible = false;
+            Mode2.Visible = false;
+            Mode3.Visible = false;
+        }
+
+        private void playerVsPlayerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gameMode = 1; //Sets the gamemode
+            Mode.Text = "Player vs. Player";
+
+            GameModeChosen();
+            Continue_btn.Visible = false;
+            //Sets up the players
+            GUIPlayer player1 = new GUIPlayer();
+            GUIPlayer player2 = new GUIPlayer();
+
+            Game g = new Game(player1, player2); //Sets up and starts the game
+            game = g; //Sets the game 
+            gameStarted = true; //Sets the game to be started
+            TurnText.Text = "Turn: " + game.CurrentPlayer; //Shows the starting player
+
+            //NextStep();
+            UpdateForm(); //Updates the form
+        }
+
+        private void playerVsAIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gameMode = 2;
+            Mode.Text = "Player vs. AI";
+
+            GameModeChosen();
+
+            GUIPlayer player = new GUIPlayer();
+            UffeSmarterAIPlayer AI = new UffeSmarterAIPlayer();
+            Game g = new Game(player, AI);
+            game = g;
+            gameStarted = true;
+            TurnText.Text = "Turn: " + game.CurrentPlayer; //Shows the starting player
+
+            //NextStep();
+            UpdateForm();
+        }
+
+        private void AIVsAIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gameMode = 3;
+            Mode.Text = "AI vs. AI";
+
+            GameModeChosen();
+
+            //Creates the AI that will be playing
+            UffeSmarterAIPlayer Ai = new UffeSmarterAIPlayer();
+            UffeAIPlayer AIDumb = new UffeAIPlayer();
+
+            Game g = new Game(AIDumb, Ai); //Creates the game with the AI
+            game = g; 
+            gameStarted = true;
+            TurnText.Text = "Turn: " + game.CurrentPlayer; //Shows the starting player
+
+            //NextStep();
+            UpdateForm();
+        }
+
+        private void Restart_btn_Click(object sender, EventArgs e)
+        {
+            switch (gameMode)
+            {
+                case 1:
+                    playerVsPlayerToolStripMenuItem_Click(sender, e);
+                    break;
+                case 2:
+                    playerVsAIToolStripMenuItem_Click(sender, e);
+                    break;
+                case 3:
+                    AIVsAIToolStripMenuItem_Click(sender, e);
+                    break;
+                default:
+                    TurnText.Text = "Choose a game (under file)";
+                    break;
+            }
+        }
+
+        private void A1_btn_Click(object sender, EventArgs e)
+        {
+            if (gameStarted)
+            {
+                Placement p = buttonsPlacements[(Button)sender];
+                if (game.IsLegalMove(p))
+                {
+                    nextP = p;
+                    NextStep();
+                }
+            }
+        }
+
+        private void Mode1_Click(object sender, EventArgs e)
+        {
+            playerVsPlayerToolStripMenuItem_Click(sender, e);
+        }
+
+        private void Mode2_Click(object sender, EventArgs e)
+        {
+            playerVsAIToolStripMenuItem_Click(sender, e);
+        }
+
+        private void Mode3_Click(object sender, EventArgs e)
+        {
+            AIVsAIToolStripMenuItem_Click(sender, e);
+        }
+    }
+}
